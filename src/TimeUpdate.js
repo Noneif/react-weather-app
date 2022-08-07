e761208ebe31972f9d8fc8c6d8f89a1e83c183f6
Noneif <sofar0222@gmail.com>
2022-08-08 01:00:45 +0300

    testing geo

--- src/TimeUpdate.js
+++ src/TimeUpdate.js
@@ -22,9 +22,7 @@ function TimeUpdate(props) {
 
   return (
     <div className="col-sm-4 mb-2 mb-sm-0 text-center text-sm-start TimeUpdate">
-      <div className="date">
-        Last update: {day} {hours}:{minutes}
-      </div>
+      Last update: {day} {hours}:{minutes}
     </div>
   );
 }
